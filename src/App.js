f6c7a6220b6b08d7411a86477d15b413e092a643
Tony Baglien <tbaglien@crescendocollective.com>
2022-07-20 11:45:16 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+
+  return (
+    <Fragment>
+      <Header />
+      {!auth && <Auth />}
+      {auth && <UserProfile />}
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
